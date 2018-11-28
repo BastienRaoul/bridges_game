3d3d931c4c3cdbfc3a7c0c39574cfe94b990e9fd
Bastien RAOUL <bastien.raoul@etu.univ-nantes.fr>
2018-11-28 10:47:37 +0100

    yo

--- ressources/vue/vueJeu.php
+++ ressources/vue/vueJeu.php
@@ -3,7 +3,10 @@
 class VueJeu {
 
   function affichagePlateau() {
-
+    echo '<h1 style="text-align:center; margin-top:2%;">Bridges</h1>';
+    echo utf8_decode('<p style="text-align:center; margin-top:2%;"><input type="button" value="Nouvelle partie"></input>
+                      <input type="button" value="Réinitialiser partie"></input>
+                      <input type="button" value="Annuler dernier mouvement"></input></p>');
   }
 }
 
