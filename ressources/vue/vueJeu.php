2e297624bf84f0ee36b528ec8c6e452d985d2d57
E164730E <sebastien.perachon@etu.univ-nantes.fr>
2018-11-28 12:21:05 +0100

    vuejeu

--- ressources/vue/vueJeu.php
+++ ressources/vue/vueJeu.php
@@ -3,10 +3,19 @@
 class VueJeu {
 
   function affichagePlateau() {
-    echo '<h1 style="text-align:center; margin-top:2%;">Bridges</h1>';
+    $html = '<html> <head> <link rel = "stylesheet" type = "text/css" href = " css.css">';
+  /*  $html .= '<h1 style="text-align:center; margin-top:2%;">Bridges</h1>';
     echo utf8_decode('<p style="text-align:center; margin-top:2%;"><input type="button" value="Nouvelle partie"></input>
                       <input type="button" value="Réinitialiser partie"></input>
                       <input type="button" value="Annuler dernier mouvement"></input></p>');
+
+*/
+    $html .= '<meta charset = "utf-8"/>';
+    $html .= '<h1>BRIDGES</h1> </head>';
+    $html .= '<body bgcolor = "#FFFFFF">';
+    $html .= '<canvas style="display: block" id="plateauCanvas" width="1000" height="1000" tabindex="1"></canvas>';
+    $html .= '<script> var c = document.getElementById("myCanvas"); var ctx = c.getContext("2d"); ctx.beginPath(); ctx.arc(100, 100, 50, 0, 2 * Math.PI); ctx.stroke(); </script>';
+    echo $html;
   }
 }
 
